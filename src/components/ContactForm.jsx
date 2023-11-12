e5d3919dcb04cd0c2ff4d11bc199fa0073d29fb2
sergiovalbuena <iosergio.valbuena@gmail.com>
2023-11-11 16:20:54 -0800

    Refactored code to improve performance and readability.

---
+++ src/components/ContactForm.jsx
@@ -0,0 +1,317 @@
+"use client";
+
+import { motion } from "framer-motion";
+import { FiCheckCircle } from "react-icons/fi";
+import { Fragment, useEffect, useRef, useState } from "react";
+
+const TerminalContact = () => {
+  const containerRef = useRef(null);
+  const inputRef = useRef(null);
+
+  return (
+    <section
+      // style={{
+      //   backgroundImage:
+      //     "url(https://images.unsplash.com/photo-1482686115713-0fbcaced6e28?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1734&q=80)",
+      //   backgroundSize: "cover",
+      //   backgroundPosition: "center",
+      // }}
+      className="px-4 py-12"
+    >
+      \{" "}
+      <div className="mb-8 flex flex-col items-start justify-between gap-4 md:flex-row md:items-end md:px-8">
+        <h2 className="max-w-lg text-4xl font-bold md:text-5xl">
+          Let&apos;s get in touch
+          <span className="text-indigo-500"> and create a better world </span>
+        </h2>
+        <div className="flex flex-col gap-2">
+          <motion.button
+            whileHover={{ scale: 1.05 }}
+            whileTap={{ scale: 0.95 }}
+            className="whitespace-nowrap rounded-lg bg-slate-900 px-4 py-2 font-medium text-white shadow-xl transition-colors hover:bg-slate-700"
+          >
+            Contact me
+          </motion.button>
+          <motion.button
+            whileHover={{ scale: 1.05 }}
+            whileTap={{ scale: 0.95 }}
+            className="whitespace-nowrap rounded-lg bg-slate-900 px-4 py-2 font-medium text-white shadow-xl transition-colors hover:bg-slate-700"
+          >
+            Email me
+          </motion.button>
+        </div>
+      </div>
+      <div
+        ref={containerRef}
+        onClick={() => {
+          inputRef.current?.focus();
+        }}
+        className="h-96 bg-slate-950/70 backdrop-blur rounded-lg w-full max-w-3xl mx-auto overflow-y-scroll shadow-xl cursor-text font-mono"
+      >
+        <TerminalHeader />
+        <TerminalBody inputRef={inputRef} containerRef={containerRef} />
+      </div>
+    </section>
+  );
+};
+
+const TerminalHeader = () => {
+  return (
+    <div className="w-full p-3 bg-slate-900 flex items-center gap-1.5 sticky top-0">
+      <div className="w-3 h-3 rounded-full bg-red-500" />
+      <div className="w-3 h-3 rounded-full bg-yellow-500" />
+      <div className="w-3 h-3 rounded-full bg-green-500" />
+      <span className="text-sm text-slate-200 font-semibold absolute left-[50%] -translate-x-[50%]">
+        sergio@desndev.com
+      </span>
+    </div>
+  );
+};
+
+const TerminalBody = ({ containerRef, inputRef }) => {
+  const [focused, setFocused] = useState(false);
+  const [text, setText] = useState("");
+
+  const [questions, setQuestions] = useState(QUESTIONS);
+
+  const curQuestion = questions.find((q) => !q.complete);
+
+  const handleSubmitLine = (value) => {
+    if (curQuestion) {
+      setQuestions((pv) =>
+        pv.map((q) => {
+          if (q.key === curQuestion.key) {
+            return {
+              ...q,
+              complete: true,
+              value,
+            };
+          }
+          return q;
+        })
+      );
+    }
+  };
+
+  return (
+    <div className="p-2 text-slate-100 text-lg">
+      <InitialText />
+      <PreviousQuestions questions={questions} />
+      <CurrentQuestion curQuestion={curQuestion} />
+      {curQuestion ? (
+        <CurLine
+          text={text}
+          focused={focused}
+          setText={setText}
+          setFocused={setFocused}
+          inputRef={inputRef}
+          command={curQuestion?.key || ""}
+          handleSubmitLine={handleSubmitLine}
+          containerRef={containerRef}
+        />
+      ) : (
+        <Summary questions={questions} setQuestions={setQuestions} />
+      )}
+    </div>
+  );
+};
+
+const InitialText = () => {
+  return (
+    <>
+      <p>Hey there! I am excited to be in conntact with you 🔗</p>
+      <p className="whitespace-nowrap overflow-hidden font-light">
+        ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+      </p>
+    </>
+  );
+};
+
+const PreviousQuestions = ({ questions }) => {
+  return (
+    <>
+      {questions.map((q, i) => {
+        if (q.complete) {
+          return (
+            <Fragment key={i}>
+              <p>
+                {q.text || ""}
+                {q.postfix && (
+                  <span className="text-violet-300">{q.postfix}</span>
+                )}
+              </p>
+              <p className="text-emerald-300">
+                <FiCheckCircle className="inline-block mr-2" />
+                <span>{q.value}</span>
+              </p>
+            </Fragment>
+          );
+        }
+        return <Fragment key={i}></Fragment>;
+      })}
+    </>
+  );
+};
+
+const CurrentQuestion = ({ curQuestion }) => {
+  if (!curQuestion) return <></>;
+
+  return (
+    <p>
+      {curQuestion.text || ""}
+      {curQuestion.postfix && (
+        <span className="text-violet-300">{curQuestion.postfix}</span>
+      )}
+    </p>
+  );
+};
+
+const Summary = ({ questions, setQuestions }) => {
+  const [complete, setComplete] = useState(false);
+
+  const handleReset = () => {
+    setQuestions((pv) => pv.map((q) => ({ ...q, value: "", complete: false })));
+  };
+
+  const handleSend = () => {
+    const formData = questions.reduce((acc, val) => {
+      return { ...acc, [val.key]: val.value };
+    }, {});
+
+    // Send this data to your server or whatever :)
+    console.log(formData);
+
+    setComplete(true);
+  };
+
+  return (
+    <>
+      <p>Beautiful! Here&#39;s what we&#39;ve got:</p>
+      {questions.map((q) => {
+        return (
+          <p key={q.key}>
+            <span className="text-blue-300">{q.key}:</span> {q.value}
+          </p>
+        );
+      })}
+      <p>Look good?</p>
+      {complete ? (
+        <p className="text-emerald-300">
+          <FiCheckCircle className="inline-block mr-2" />
+          <span>Sent! We&#39;ll get back to you ASAP 😎</span>
+        </p>
+      ) : (
+        <div className="flex gap-2 mt-2">
+          <button
+            onClick={handleReset}
+            className="px-3 py-1 text-base hover:opacity-90 transition-opacity rounded bg-slate-100 text-black"
+          >
+            Restart
+          </button>
+          <button
+            onClick={handleSend}
+            className="px-3 py-1 text-base hover:opacity-90 transition-opacity rounded bg-indigo-500 text-white"
+          >
+            Send it!
+          </button>
+        </div>
+      )}
+    </>
+  );
+};
+
+const CurLine = ({
+  text,
+  focused,
+  setText,
+  setFocused,
+  inputRef,
+  command,
+  handleSubmitLine,
+  containerRef,
+}) => {
+  const scrollToBottom = () => {
+    if (containerRef.current) {
+      containerRef.current.scrollTop = containerRef.current.scrollHeight;
+    }
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    handleSubmitLine(text);
+    setText("");
+    setTimeout(() => {
+      scrollToBottom();
+    }, 0);
+  };
+
+  const onChange = (e) => {
+    setText(e.target.value);
+    scrollToBottom();
+  };
+
+  useEffect(() => {
+    return () => setFocused(false);
+  }, []);
+
+  return (
+    <>
+      <form onSubmit={onSubmit}>
+        <input
+          ref={inputRef}
+          onChange={onChange}
+          value={text}
+          type="text"
+          className="sr-only"
+          autoComplete="off"
+          onFocus={() => setFocused(true)}
+          onBlur={() => setFocused(false)}
+        />
+      </form>
+      <p>
+        <span className="text-emerald-400">➜</span>{" "}
+        <span className="text-cyan-300">~</span>{" "}
+        {command && <span className="opacity-50">Enter {command}: </span>}
+        {text}
+        {focused && (
+          <motion.span
+            animate={{ opacity: [1, 1, 0, 0] }}
+            transition={{
+              repeat: Infinity,
+              duration: 1,
+              ease: "linear",
+              times: [0, 0.5, 0.5, 1],
+            }}
+            className="inline-block w-2 h-5 bg-slate-400 translate-y-1 ml-0.5"
+          />
+        )}
+      </p>
+    </>
+  );
+};
+
+export default TerminalContact;
+
+const QUESTIONS = [
+  {
+    key: "email",
+    text: "To start, could you give us ",
+    postfix: "your email?",
+    complete: false,
+    value: "",
+  },
+  {
+    key: "name",
+    text: "Awesome! And what's ",
+    postfix: "your name?",
+    complete: false,
+    value: "",
+  },
+  {
+    key: "description",
+    text: "Perfect, and ",
+    postfix: "how can we help you?",
+    complete: false,
+    value: "",
+  },
+];
