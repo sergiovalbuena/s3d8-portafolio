99504bbc0ab36817474528028f0b4771d7827a68
sergiovalbuena <iosergio.valbuena@gmail.com>
2023-11-11 16:12:54 -0800

    Add BouncyCardsFeatures component to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Hero from "../components/Hero";
+import BouncyCardsFeatures from "../components/BouncyCardsFeatures";
 
 export default function Home() {
   return (
     <main className="">
       Hello
       <Hero />
+      <BouncyCardsFeatures />
     </main>
   );
 }
