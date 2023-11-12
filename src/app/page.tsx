f3c711df591b5fc1d40f71dc43d964c5fef706be
sergiovalbuena <iosergio.valbuena@gmail.com>
2023-11-11 16:40:37 -0800

    Add Modal component to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Hero from "../components/Hero";
 import BouncyCardsFeatures from "../components/BouncyCardsFeatures";
 import ContactForm from "../components/ContactForm";
+import Modal from "../components/Modal";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       Hello
       <Hero />
       <BouncyCardsFeatures />
+      <Modal />
       <ContactForm />
     </main>
   );
