b2547af7611dc9dbbff47da4882e8d9dd85ee9c7
sergiovalbuena <iosergio.valbuena@gmail.com>
2024-01-30 20:58:23 -0800

    Add Zombie Game project images

--- src/app/projects/kitchen-flow/page.jsx
+++ src/app/projects/kitchen-flow/page.jsx
@@ -14,81 +14,86 @@ export default function KitchenflowPage() {
 
 const products = [
   {
-    title: "Moonbeam",
+    title: "Home Page",
     link: "",
-    thumbnail: "/projects/ZombieGame.png",
+    thumbnail: "/projects/kf/kf1.png",
   },
   {
-    title: "Cursor",
+    title: "Pricing",
     link: "",
-    thumbnail: "/projects/ZombieGame.png",
+    thumbnail: "/projects/kf/kf2.png",
   },
   {
-    title: "Rogue",
+    title: "Features",
     link: "",
-    thumbnail: "/projects/ZombieGame.png",
-  },
-
-  {
-    title: "Editorially",
-    link: "",
-    thumbnail: "/projects/ZombieGame.png",
-  },
-  {
-    title: "Editrix AI",
-    link: "",
-    thumbnail: "/projects/ZombieGame.png",
-  },
-  {
-    title: "Pixel Perfect",
-    link: "",
-    thumbnail: "/projects/ZombieGame.png",
+    thumbnail: "/projects/kf/kf3.png",
   },
 
   {
-    title: "Algochurn",
+    title: "Pledge",
     link: "",
-    thumbnail: "/projects/ZombieGame.png",
+    thumbnail: "/projects/kf/kf4.png",
   },
   {
-    title: "Aceternity UI",
+    title: "Team",
     link: "",
-    thumbnail: "/projects/ZombieGame.png",
+    thumbnail: "/projects/kf/kf5.png",
   },
   {
-    title: "Tailwind Master Kit",
+    title: "Contact",
     link: "",
-    thumbnail: "/projects/ZombieGame.png",
-  },
-  {
-    title: "SmartBridge",
-    link: "",
-    thumbnail: "/projects/ZombieGame.png",
-  },
-  {
-    title: "Renderwork Studio",
-    link: "",
-    thumbnail: "/projects/ZombieGame.png",
+    thumbnail: "/projects/kf/kf6.png",
   },
 
   {
-    title: "Creme Digital",
+    title: "KitchenMetric Engine",
     link: "",
-    thumbnail: "/projects/ZombieGame.png",
+    thumbnail: "/projects/kf/kf7.png",
+  },
+  {
+    title: "Work Smarter",
+    link: "",
+    thumbnail: "/projects/kf/kf8.png",
+  },
+  {
+    title: "Employee List",
+    link: "",
+    thumbnail: "/projects/kf/kf9.png",
+  },
+  {
+    title: "Chef AI - ChatGPT",
+    link: "",
+    thumbnail: "/projects/kf/kf10.png",
+  },
+  {
+    title: "Updating Task",
+    link: "",
+    thumbnail: "/projects/kf/kf11.png",
+  },
+
+  {
+    title: "Recipes employee View",
+    link: "",
+    thumbnail: "/projects/kf/kf12.png",
   },
   {
     title: "Golden Bells Academy",
     link: "",
-    thumbnail: "/projects/ZombieGame.png",
+    thumbnail: "/projects/kf/kf13.png",
   },
   {
-    title: "Invoker Labs",
+    title: "Recipes Employee View",
     link: "",
-    thumbnail: "/projects/ZombieGame.png",
+    thumbnail: "/projects/kf/kf14.png",
   },
   {
-    title: "E Free Invoice",
+    title: "Task List",
     link: "",
-    thumbnail: "/projects/ZombieGame.png",
+    thumbnail: "/projects/kf/kf15.png",
+  },
+  {
+    title: "Waste Reports",
+    link: "",
+    thumbnail: "/projects/kf/kf16 .png",
   },
 ];
---
+++ src/app/projects/zombie-game/page.jsx
@@ -0,0 +1,91 @@
+import { HeroParallax } from "@/components/ui/hero-parallax";
+
+export default function ZombieGamePage() {
+  return (
+    <div>
+      <HeroParallax
+        products={products}
+        headerTitle="Zombie Game"
+        headerDescription="A zombie game made with Unity."
+      />
+    </div>
+  );
+}
+
+const products = [
+  {
+    title: "Start Screen",
+    link: "",
+    thumbnail: "/projects/zg/zg1.png",
+  },
+  {
+    title: "Gameplay",
+    link: "",
+    thumbnail: "/projects/zg/zg2.png",
+  },
+  {
+    title: "Gameplay",
+    link: "",
+    thumbnail: "/projects/zg/zg3.png",
+  },
+  {
+    title: "Gameplay",
+    link: "",
+    thumbnail: "/projects/zg/zg4.png",
+  },
+  {
+    title: "Gameplay",
+    link: "",
+    thumbnail: "/projects/zg/zg5.png",
+  },
+  {
+    title: "Gameplay",
+    link: "",
+    thumbnail: "/projects/zg/zg6.png",
+  },
+  {
+    title: "Gameplay",
+    link: "",
+    thumbnail: "/projects/zg/zg7.png",
+  },
+  {
+    title: "Gameplay",
+    link: "",
+    thumbnail: "/projects/zg/zg8.png",
+  },
+  {
+    title: "Gameplay",
+    link: "",
+    thumbnail: "/projects/zg/zg9.png",
+  },
+  {
+    title: "Gameplay",
+    link: "",
+    thumbnail: "/projects/zg/zg10.png",
+  },
+  {
+    title: "Gameplay",
+    link: "",
+    thumbnail: "/projects/zg/zg11.png",
+  },
+  {
+    title: "Gameplay",
+    link: "",
+    thumbnail: "/projects/zg/zg12.png",
+  },
+  {
+    title: "Gameplay",
+    link: "",
+    thumbnail: "/projects/zg/zg14.png",
+  },
+  {
+    title: "Gameplay",
+    link: "",
+    thumbnail: "/projects/zg/zg15.png",
+  },
+  {
+    title: "Gameplay",
+    link: "",
+    thumbnail: "/projects/zg/zg16.png",
+  },
+];
